162c9ab6550947ad35b611af5f09a2f3b5967dea
shjan <sheilaqasemi2018@gmail.com>
2018-12-05 22:20:41 +0100

    Age-ify (A future age calculator)

---
+++ javascript/javascript1/week1/part4_smart_ease.js
@@ -0,0 +1,8 @@
+
+//part 4 - working in smart ease
+
+//Age-ify (A future age calculator)
+let yearFuture = 2027;
+let yearOfBirth = 1987;
+let age = yearFuture - yearOfBirth;
+console.log("You will be " + age + " years old in " + yearFuture);
