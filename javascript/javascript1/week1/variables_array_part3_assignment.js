d3166ca826119359fd1aabeb0fd2d9490f57af25
shjan <sheilaqasemi2018@gmail.com>
2018-12-05 20:11:57 +0100

    working with arrays

--- javascript/javascript1/week1/variables_array_part3_assignment.js
+++ javascript/javascript1/week1/variables_array_part3_assignment.js
@@ -5,7 +5,26 @@
 
 // round to show the nearst number
    let num = 7.25;
-   var nearst_num = Math.round(num);
+   let nearst_num = Math.round(num);
+
+// an empty array declaration
+   let foods = [];
+
+ // A sentence to shows the importance of array to me
+   console.log("It is used to represent multiple data items of same type by using only single name.");
+
+ // array in console.log
+  console.log(foods);
+
+//Array to shows my favorite animals
+   let myFavoriteAnimals = ["cat", "rabbit", "dog"];
+
+// to log array
+   console.log(myFavoriteAnimals);
+//to add another element to existence array
+   myFavoriteAnimals.push("baby pig");
+ // to log that
+   console.log(myFavoriteAnimals);
 	
 
 
