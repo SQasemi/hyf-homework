9b363c5e8daefe0fe632efc5978bb8f94a6fa5e6
shjan <sheilaqasemi2018@gmail.com>
2018-12-06 20:19:50 +0100

    house price

---
+++ javascript/javascript1/week1/part4_house_price.js
@@ -0,0 +1,9 @@
+//part4_house_price
+
+let width = 8 ;
+let height = 10;
+let dept = 10 ;
+let gardenSizeInM2 = 100;
+let volumeInMeters = width * height * dept;
+let housePrice = volumeInMeters*2.5*1000+gardenSizeInM2*300;
+console.log(housePrice);
