e181aba95d743b2ed29e44728f3ca808f5cb12a5
shjan <sheilaqasemi2018@gmail.com>
2018-12-06 14:16:59 +0100

    calculation of dog's age...

---
+++ javascript/javascript1/week1/part4_dogs_age_calculation.js
@@ -0,0 +1,14 @@
+//to calculate age of dog
+
+let dogYearOfBirth = 2017 ; 
+let dogYearFuture = 2027 ;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears;
+if(shouldShowResultInDogYears)
+{
+    console.log("your dog will be " + (dogYear*7) + " years old of dogs old in " + dogYearFuture);
+}
+ else 
+{
+     console.log("your dog will be " + dogYear + " human years old in " + dogYearFuture);
+}
