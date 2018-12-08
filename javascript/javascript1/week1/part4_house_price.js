512bdcb2a2295f7f8df8ba7933263999efb7ff72
shjan <sheilaqasemi2018@gmail.com>
2018-12-08 12:04:45 +0100

    small changes

--- javascript/javascript1/week1/part4_house_price.js
+++ javascript/javascript1/week1/part4_house_price.js
@@ -7,3 +7,9 @@ let gardenSizeInM2 = 100;
 let volumeInMeters = width * height * dept;
 let housePrice = volumeInMeters*2.5*1000+gardenSizeInM2*300;
 console.log(housePrice);
+if( housePrice<=2500000){
+console.log("its a good buy for Peter");
+}else
+{
+console.log("its not a good buy for Peter");
+}
