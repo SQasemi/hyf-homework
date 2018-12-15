c8e43ec0e3bd4b44a9f2d2730b802c26525a2e70
shjan <sheilaqasemi2018@gmail.com>
2018-12-15 14:55:57 +0100

    saving, getting and displaying notes

--- javascript/javascript1/week2/smart_ease_note_taking_app.js
+++ javascript/javascript1/week2/smart_ease_note_taking_app.js
@@ -2,15 +2,43 @@
 //save a note 
 let notes = [];
 let numberOfNotes;
+note = "dd";
+noteId = 2;
 function addNote(note, noteId)
 {
-    note = "";
-    noteId = 2;
+   
     let noteobj = {
-        note : noteId
+        note : note,
+        noteId: noteId
+        
     }
     notes.push(noteobj);
     numberOfNotes = notes.length;
 }
 addNote("fgg", 1);
 console.log(numberOfNotes);
+
+
+
+//to get note
+function getNoteFromId(id){
+    for(let i=0; i <= notes.length; i++){
+        if(id === notes[i].noteId){
+            return notes[i];
+        }
+        else{
+            return "Opps!"; 
+        }
+    }
+}
+let getNote =getNoteFromId(1);
+console.log(getNote);
+
+//to read notes
+function showAllNotes(){
+for(let i = 0; i < notes.length; i++)
+{
+    console.log("The note with id = " + notes[i].noteId + " has the following note = " + notes[i].note);
+}
+}
+showAllNotes();
