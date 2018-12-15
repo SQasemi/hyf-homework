3efc6a933eebe282ab5a1cd657e88ce07250a369
shjan <sheilaqasemi2018@gmail.com>
2018-12-15 13:38:03 +0100

    working with date function to determine meeting day

--- javascript/javascript1/week2/warmup_part2_7.js
+++ javascript/javascript1/week2/warmup_part2_7.js
@@ -30,3 +30,43 @@ console.log(colorObjects);
 for(var i=0; i<arrObjColor.lenght; i++){
     console.log(arrObjColor[i]);
 }
+
+
+//8: how to determin exact name of day from number of days
+
+function findDay(n ){
+    let date = new Date();
+    let day = new Array(7);
+    day[0] = "Sunday";
+    day[1] = "Monday";
+    day[2] = "Tuesday";
+    day[3] = "Wednsday"
+    day[4] = "Thursday";
+    day[5] = "friday";
+    day[6] = "saturday";
+ let numbersOfDay = day[date.getDay()];
+ let exactDay = n % 7 ;
+console.log(exactDay);
+console.log("today is sunday");
+console.log("How many days to meet " +n);
+console.log("the meeting day is " +day[exactDay]);
+
+}
+
+//To call function
+findDay(9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
