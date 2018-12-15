b0b9b961ed2f347f5ee8f62216ada566a0f16d47
shjan <sheilaqasemi2018@gmail.com>
2018-12-15 18:21:52 +0100

    adding activity

--- javascript/javascript1/week2/cactusIO.js
+++ javascript/javascript1/week2/cactusIO.js
@@ -13,3 +13,10 @@ function addActivity(date, activity, duration){
  addActivity("23-dec-2018","youtube",21);
  addActivity("24-dec-2018","google",6);
  console.log(activities);
+
+
+ //show my status
+ function showStatus(){
+     for (let i = 0; i< activities.length; i++)
+     console.log("");
+ }
