80f1bbdca66b95f0dc3b0fe9ed58671846c2c093
shjan <sheilaqasemi2018@gmail.com>
2019-01-04 19:51:46 +0100

    some changes

---
+++ javascript/javascript1/week3/4_creating_obj.js
@@ -0,0 +1,63 @@
+//4: creating object for each book
+let booksObj = {
+            "romeo_and_juliet": {
+              "title": "Romeo And Juliet",
+              "language": "english",
+              "author" :"williem" 
+
+            },
+            "war_and_peace":{
+              "title": "War and Peace",
+              "language": "english",
+              "author" :"Leo Tolstoy" 
+                          
+            },
+            "don_quixote":{
+              "title": "Don Quixote",
+              "language": "english",
+              "author" :"Miguel de Cervantes" 
+                          
+            },
+            "hamlet":{
+              "title": "Hamlet",
+              "language": "english",
+              "author" :"Erminio Macario" 
+                          
+            },
+            "in_search_of_lost_time":{
+              "title": "In search of lost time",
+              "language": "english",
+              "author" :" Marcel Proust" 
+                          
+            },
+            "programming_foundemental":{
+              "title": "Programming foundemental",
+              "language": "english",
+              "author" :"D.E Stevenson" 
+                          
+            },
+            "safid_barfi":{
+              "title": "Safid barfi",
+              "language": "farsi",
+              "author" :"Lisa" 
+                          
+            },
+            "nineteen_eighty_four":{
+              "title": "nineteen eighty four",
+              "language": "english",
+              "author" :"" 
+                          
+            },
+            "innevetators":{
+              "title": "Innevetators",
+              "language": "english",
+              "author" :"" 
+                          
+            },
+            "basic_javascript":{
+              "title": "Basic javascript",
+              "language": "english",
+              "author" :"" 
+                          
+            },
+          } 
