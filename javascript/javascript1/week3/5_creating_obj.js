9d4213cf76f8bcff5dac1a0835151edd39b705ed
shjan <sheilaqasemi2018@gmail.com>
2019-01-06 08:58:15 +0100

    small change

--- javascript/javascript1/week3/5_creating_obj.js
+++ javascript/javascript1/week3/5_creating_obj.js
@@ -1,136 +1,105 @@
-//5,6,7 creating objects,function to display that ans some css style
-let booksObj = {
-    1: {
-        "title": "",
-        "language": "",
-        "author": ""
+
+
+// make an object with info for each book
+let bookInfo = [{
+        id: "romeo_and_juliet",
+        title: "Romeo And Juliet",
+        language: "english",
+        author :"williem"
     },
-    2: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "war_and_peace",
+        title: "War and Peace",
+        language: "english",
+        author :"Leo Tolstoy" 
     },
-    3: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "don_quixote",
+        title: "Don Quixote",
+        language: "english",
+        author :"Miguel de Cervantes"
     },
-    4: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "hamlet",
+        title: "Hamlet",
+        language: "english",
+        author :"Erminio Macario"
     },
-    5: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "in_search_of_lost_time",
+        title: "In search of lost time",
+        language: "english",
+        author :" Marcel Proust"
     },
-    6: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "programming_foundemental",
+        title: "Programming foundemental",
+        language: "english",
+        author :"D.E Stevenson" 
     },
-    7: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "safid_barfi",
+        title: "Safid barfi",
+        language: "farsi",
+        author :"Lisa" 
     },
-    8: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "nineteen_eighty_four",
+        title: "nineteen eighty four",
+        language: "english",
+        author :"" 
     },
-    9: {
-        "title": "",
-        "language": "",
-        "author": ""
+    {
+        id: "innevetators",
+        title: "Innevetators",
+        language: "english",
+        author :"" 
     },
-    10: {
-        "title": "",
-        "language": "",
-        "author": ""
-    },
-};
-let booksWriter = [
-    "williem",
-    "Leo Tolstoy",
-    "Miguel de Cervantes",
-    "Erminio Macario",
-    "Marcel Proust",
-    "D.E Stevenson",
-    "Lisa",
-    " ",
-    " ",
-    " "
-];
-let booksName = [
-    "Romeo And Juliet",
-    "war and peace",
-    "Don Quixote",
-    "Hamlet",
-    "In search of lost time",
-    "Programming foundemental",
-    "Safid barfi",
-    "nineteen eighty four",
-    "Innevetators",
-    "Basic javascript"
-];
-let booksImage = [
-    "images/Romeo and Juliet.jpeg",
-    "images/war_and_peace.jpg",
-    "images/don_quixote.jpg",
-    "images/hamlet.jpg",
-    "images/in search of lost time.jpeg",
-    "images/programming foundemental.jpg",
-    "images/safid barfi.jpeg",
-    "images/9984.jpg",
-    "images/ ",
-    "images/basicjs.png"
-];
-let bookslang = [
-    "english", "english", "english",
-    "english", "english", "english",
-    "farsi", "english", "english",
-    "english"
-];
-let index = 0;
-let jIndex = -1;
-for (let key in booksObj) {
-    jIndex = jIndex + 1;
-    index = index + 1;
-    stringIndex = index.toString();
-    if (key === stringIndex) {
-        booksObj[key]['title'] = booksName[jIndex];
-        booksObj[key]['language'] = bookslang[jIndex];
-        booksObj[key]['author'] = booksWriter[jIndex];
+    {
+        id: "basic_javascript",
+        title: "Basic javascript",
+        language: "english",
+        author :"" 
     }
-};
-index = -1;
-makeListBook();
-function makeListBook() {
-    for (let propt in booksObj) {
-        index = index + 1;
-        document.body.style.border = "5px solid green";
-        let bookInTable = document.createElement('ul');
-        let bookTitle = document.createElement('h1');
-        let titleText = document.createTextNode(booksObj[propt]['title']);
-        bookTitle.appendChild(titleText);
-        let bookLanguage = document.createElement('li');
-        let languageText = document.createTextNode(booksObj[propt]['language']);
-        bookLanguage.appendChild(languageText);
-        let bookAuthor = document.createElement('li');
-        let authorText = document.createTextNode(booksObj[propt]['author']);
-        bookAuthor.appendChild(authorText);
-        bookInTable.appendChild(bookAuthor);
-        bookInTable.appendChild(bookLanguage);
-        let bookPic = document.createElement('li');
-        let img = document.createElement('img');
-        img.style.height = '150px'
-        img.src = booksImage[index];
-        bookPic.appendChild(img);
-        bookInTable.appendChild(bookPic);
-        document.body.appendChild(bookTitle);
-        document.body.appendChild(bookInTable);
-       
+]
+
+//5.  to take actual information from the object and display it
+function bookList() {
+    document.body.style.border = "5px solid green";
+    let x = document.createElement("ul");
+    document.body.appendChild(x);
+    for (let i = 0; i < books.length; i++) {
+        let list = document.createElement("li");
+        x.appendChild(list);
+        list.setAttribute("id", books[i]); 
+        let title = document.createElement("h1");
+        title.innerHTML = bookInfo[i].title;
+        list.appendChild(title);
+        let author = document.createElement("p");
+        author.innerHTML = bookInfo[i].author;
+        list.appendChild(author);
+        let language = document.createElement("p");
+        language.innerHTML = bookInfo[i].language;
+        list.appendChild(language);
+        
     }
-};
+}
+bookList();
+
+//new object with book covers
+let booksImage = {
+    "romeo_and_juliet": "./images/Romeo and Juliet.jpeg",
+    "war_and_peace":"images/war_and_peace.jpg",
+    "don_quixote":"images/don_quixote.jpg",
+    "hamlet":"images/hamlet.jpg",
+    "in_search_of_lost_time":"images/in search of lost time.jpeg",
+    "programming_foundemental":"images/programming foundemental.jpg",
+    "safid_barfi":"images/safid barfi.jpeg",
+    "nineteen_eighty_four":"images/9984.jpg",
+    "innevetators":"images/ ",
+    "basic_javascript":"images/basicjs.png"
+}
+
+
+
+
