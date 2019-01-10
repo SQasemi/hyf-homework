22566822c7ed4bf30f7418ce4c75b81b4099812c
shjan <sheilaqasemi2018@gmail.com>
2019-01-10 17:58:40 +0100

    gernating spirit animal

---
+++ javascript/javascript2/week1/script.js
@@ -0,0 +1,28 @@
+//to click button
+let button = document.getElementById("displaySpriritAnimal");
+button.addEventListener("click", function(){
+	let textFeild = document.getElementById("animal").value;
+	//alert (animal);
+
+	//to create random Items
+	let randomItem = Math.floor(Math.random() * 10);
+	//console.log(randomItem);
+
+    let spiritAnimalArea = document.getElementById("spiritAnimal");
+    spiritAnimalArea.innerHTML = textFeild + "-" + spritiAnimalArray[randomItem];
+    
+});
+
+//spirit animal array
+let spritiAnimalArray = [
+ "Obsidian ape of Kovo",
+ "Marble swan of Ninani",
+ "Silver wolf of Briggan",
+ "Glass polar Bear or Suka",
+ "Jade serpent of Generation",
+ "Bronze eagle of Halawir",
+ "Bambo panda of Jhi",
+ "Platinum Elk of Tellun",
+ "Iron Boar Of Rumfuss",
+ "Golden Lion Of Cabaro"
+]; 
