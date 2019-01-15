db35e1e1bcfc7f256607601dc2fa54f59ff7fa0f
shjan <sheilaqasemi2018@gmail.com>
2019-01-15 22:33:49 +0100

    adding comments into scripts

--- javascript/javascript2/week2/main.js
+++ javascript/javascript2/week2/main.js
@@ -1,11 +1,29 @@
-function threeFive(startIndex, stopIndex){
-    //declare an array
-    numArray = [];
+
+let numArray = [];
+let i;
+let threeFive = function (startIndex, stopIndex, threeCallback, fiveCallback){
+   
     //iterate by using for loop
-    for(let i = startIndex ;i <= stopIndex; i++ ){
-        numArray.push(i);
+    for( i = startIndex; i <= stopIndex; i++ ){
+        if(threeCallback && fiveCallback){
+            sayThree();
+            sayFive();
+        }
+        numArray.push(i);    
+       
     }
     return numArray;
 };
+
+//to calculate the divisible by three
+let sayThree = function(){
+ return (numArray[i] % 3 === 0);
+}
+
+//to calculate divisible by five
+let sayFive = function(){
+    return (numArray[i] % 5 ===0)
+}
 //example
-console.log(threeFive(10,15));
+console.log(threeFive(10,15,sayThree,sayFive));
+//console.log(threeFive(sayThree,sayFive));
