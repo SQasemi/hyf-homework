c7fbfa845fe66f1b226406ba0fc8504ac29c191f
SQasemi <44619858+SQasemi@users.noreply.github.com>
2019-01-15 16:59:23 +0100

    creating array and for loop

---
+++ javascript/javascript2/week2/main.js
@@ -0,0 +1,11 @@
+function threeFive(startIndex, stopIndex){
+    //declare an array
+    numArray = [];
+    //iterate by using for loop
+    for(let i = startIndex ;i <= stopIndex; i++ ){
+        numArray.push(i);
+    }
+    return numArray;
+};
+//example
+console.log(threeFive(10,15));
