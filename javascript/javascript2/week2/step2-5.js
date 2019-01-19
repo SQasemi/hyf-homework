697dd28f7de333ce15f5f595af5e0578552d9cc3
SQasemi <44619858+SQasemi@users.noreply.github.com>
2019-01-19 21:03:44 +0100

    working with geolocation

--- javascript/javascript2/week2/step2-5.js
+++ javascript/javascript2/week2/step2-5.js
@@ -29,7 +29,45 @@ const showSomthing = () => {
 function doSomething () {
  showSomthing();
 }
+//to call them 
+doSomething();
 
-doSomething(showSomthing);
+//creating object to have function as value of its key
+functionObj = {
+    numfunc : () => {
+        console.log("a simple function inside object ")
+    }
+}
+//to call a function that is inside obj
+functionObj.numfunc();
 
-//
+
+//two setTimeOut to uses one function
+const setTimeOutFunc = () => {
+    setTimeout (function () {
+        console.log("Be passion!")
+    },1000);
+
+    setTimeout (function(){
+        console.log("Think positive! ");
+    },2000);
+
+
+}
+setTimeOutFunc();
+
+
+
+//outer funtions and inner functions
+let outerFunction = (name) => {
+
+    let innerFunction = () =>{
+     return name;
+    }
+    return innerFunction;
+}
+
+let firstName = outerFunction("Sheila");
+console.log(firstName);
+
+
