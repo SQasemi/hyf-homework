4301b2bcc1124861651685ce787be609a8a5deda
user <sheilaqasemi2018@gmail.com>
2019-02-12 01:49:34 +0100

    creating array of objects to show list of items according thumbs..

--- javascript/javascript3/week2/Thumbs up or thumbs down/main.js
+++ javascript/javascript3/week2/Thumbs up or thumbs down/main.js
@@ -1,36 +1,99 @@
-//when the buttons clicked
-let rejectBtn = document.querySelector('.reject');
-rejectBtn.addEventListener('click', () =>{
-    let firstLi = document.querySelector('li');
-    firstLi.style.transform ="translateX(-1000px)";
-    // let nextLi = document.getElementById('l2');
-    // nextLi.style.opacity = "1";
-    // nextLi.style.transform = "scale(1)"; 
-  
-});
 
-// let acceptBtn = document.querySelector('.accept');
-// acceptBtn.addEventListener('click', () =>{
-// //  let firstLi = document.getElementsByTagName('ul')[0];
-// let firstLi = document.getElementById('l1');
-//  firstLi.style.transform ="translateX(1000px)";
-
-//  let nextLi = document.getElementById('l2');
-//   nextLi.style.opacity = "1";
-//   nextLi.style.transform = "scale(1)"; 
-
-// });
-
-
-
+//thumb up and down with use of promise
+let listItems = document.querySelectorAll('li');
+let indexCounnterOfLi = 0;
+//when the accept button is clicked
 let acceptBtn = document.querySelector('.accept');
 acceptBtn.addEventListener('click', () =>{
-//  let firstLi = document.getElementsByTagName('ul')[0];
- let firstLi = document.getElementById('l1');
- firstLi.style.transform ="translateX(1000px)";
-
- let nextLi = document.getElementById('l2');
-   nextLi.style.opacity = "1";
-  nextLi.style.transform = "scale(1)"; 
-
+    animateLiOut(indexCounnterOfLi, 'right')
+    .then (() =>{
+       animateNextLi(indexCounnterOfLi); 
+    })
+    indexCounnterOfLi++;
 });
+
+//when the reject button is clicked
+let rejectBtn = document.querySelector('.reject');
+rejectBtn.addEventListener('click', () =>{
+    animateLiOut(indexCounnterOfLi, 'left')
+    .then (() =>{
+       animateNextLi(indexCounnterOfLi); 
+    })
+    indexCounnterOfLi++;
+});
+//function to define the direction 
+function animateLiOut(index, changeDirection){
+    if(changeDirection === 'left'){
+        listItems[index].style.transform = 'translateX(-1000px)';
+    }
+    else{
+        listItems[index].style.transform = 'translateX(1000px)'; 
+    }
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve();
+        },700);
+    });
+}
+
+function animateNextLi (index) {
+    listItems[index].style.transform = 'scale(1)';
+        listItems[index].style.opacity = 1;
+}
+
+
+//Personalise the application
+//array of objects
+let famousFootballPlayers = [
+    {
+       name: 'Cristiano Ronaldo',
+       age: 34,
+       country:'Portuguese',
+       photo:'famousFootballPlayers/ch.jpg'
+    },
+    {
+        name: 'Lionel Messi',
+        age: 31,
+        country:'Argentina',
+        photo:'famousFootballPlayers/messi.JPG'
+     },
+     {
+        name: 'Emiliano Sala',
+        age: 28,
+        country:'Argentina',
+        photo:'famousFootballPlayers/a.jpg'
+     },
+     {
+        name: 'Sergio Agüero',
+        age: 28,
+        country:'Argentina',
+        photo:'./famousFootballPlayers/sa.jpg'
+        
+     },
+     {
+        name: 'Karim Benzema ',
+        age: 30,
+        country:'Argentina',
+        photo:'./famousFootballPlayers/bnz.jpg'
+        
+     }
+     
+] 
+
+//to create a list of Items
+ for(let items = 0; items <= famousFootballPlayers.length; items++){
+ // console.log(listItems);
+  listItems[items].innerHTML = "";
+  let addtitle = document.createElement('h6');
+  listItems[items].appendChild(addtitle);
+  addtitle.innerHTML = famousFootballPlayers[items].name;
+
+  let addAge = document.createElement('h5');
+  listItems[items].appendChild(addAge);
+  addAge.innerHTML = famousFootballPlayers[items].age;
+
+  let addPhoto = document.createElement('img');
+  listItems[items].appendChild(addPhoto);
+  addPhoto.src = famousFootballPlayers[items].photo;
+  
+}
