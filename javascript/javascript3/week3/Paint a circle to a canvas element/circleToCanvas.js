df665f09fbd00a5898005da9e05f8f0b1b995cf8
user <sheilaqasemi2018@gmail.com>
2019-02-15 00:50:53 +0100

    add random circle in canvas

--- javascript/javascript3/week3/Paint a circle to a canvas element/circleToCanvas.js
+++ javascript/javascript3/week3/Paint a circle to a canvas element/circleToCanvas.js
@@ -33,20 +33,14 @@ class Circle {
         this.startAngle = startAngle;
         this.endAngle = endAngle;
         this.fillColor = fillColor;
-
-     
-
     }
     draw(canvas){
          
           canvas = document.querySelector('.add_canvas');
          if(canvas.getContext){
         let ctx = canvas.getContext('2d');
-       // for(let i=0; i<1000; i++){
-       // ctx.strokeStyle = "#" + Math.floor(Math.random()*0xFFFFFF).toString(16);
         ctx.beginPath();
-       //ctx.arc(Math.floor(Math.random()*(100)+1),Math.floor(Math.random()*(80)+1),Math.floor(Math.random()*(20)+1),0,2*Math.PI );
-        ctx.arc(this.x, this.y, this.r,0, this.fillColor);
+        ctx.arc(this.x, this.y, this.r,this.startAngle, this.endAngle);
         ctx.stroke();
         //to give color for circle inside canvas
         ctx.fillStyle = this.fillColor;
@@ -61,16 +55,39 @@ class Circle {
 
 }
 
-const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, 'red');
+ let c1 = new Circle(100, 100, 20, 0, 2 * Math.PI, 'red');
 console.log(c1)
 c1.draw();
- const c3 = new Circle(45, 45, 15, 0, 2 * Math.PI, 'blue');
+let c3 = new Circle(45, 45, 15, 0, 2 * Math.PI, 'blue');
 c3.draw();
-const c2 = new Circle(50, 50, 20, 0, 4 * Math.PI, 'green');
+let c2 = new Circle(50, 50, 20, 0, 4 * Math.PI, 'green');
 console.log(c2)
 c2.draw();
 
+//random
+function randomCircle(){
+    setTimeout(()=>{
 
-
-
+    let x = Math.random() * 200;
+    let y = Math.random() * 100;
+    let r = Math.random() * 20;
+    //random color
+   
+    function randColor2() {
+        let r = ('0'+(Math.random()*256|0).toString(16)).slice(-2),
+            g = ('0'+(Math.random()*256|0).toString(16)).slice(-2),
+            b = ('0'+(Math.random()*256|0).toString(16)).slice(-2);
+        return '#' +r+g+b;
+    }
+   
+   
+        c1 = new Circle(x, y, r, 0, 2 * Math.PI, randColor2());
+        c1.draw(); 
+    
+    },1000)
+}
+    //to loop function
+    for(let c=0 ; c<= 1000 ; c++){
+        randomCircle(c);
+    }
 
